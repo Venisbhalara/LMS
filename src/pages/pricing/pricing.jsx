c5fc6b26ccd182b4622f45ee29e6a11fc60630be
Venis <vasubhalara44@gmail.com>
2026-01-09 17:17:40 +0530

    Update pricing.jsx

--- src/pages/pricing/pricing.jsx
+++ src/pages/pricing/pricing.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Pricing.css";
+import "./pricing.css";
 import PaymentModal from "./PaymentModal";
 
 const Pricing = () => {
